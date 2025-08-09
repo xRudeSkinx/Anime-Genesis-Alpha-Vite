8eeb2d698a72adec8fdaf0ca1878cdbd656caf8b
xRudeSkinx <justrude10@gmail.com>
2025-08-08 21:59:50 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import Gencore from './modules/Gencore.jsx'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/gencore" element={<Gencore />} />
+    </Routes>
+  </HashRouter>
+)
 
