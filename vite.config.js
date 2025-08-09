019584cde90614887f11b7079568bbfe998d0c3d
xRudeSkinx <justrude10@gmail.com>
2025-08-08 21:25:55 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  // base is empty on Vercel
+  base: '/',
+  plugins: [react()]
+})
